4137f30e7629fc26bb8f54ae2072ab47c6309cb2
makoban <banmakodh@gmail.com>
2026-01-28 11:12:07 +0900

    Fix ESLint config and normalize filenames in experimental folder to resolve CI issues

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,3 +1,17 @@
 import { config } from "@remotion/eslint-config-flat";
 
-export default config;
+export default [
+  ...config,
+  {
+    ignores: [
+      "**/予想/**",
+      "**/事業構築/**",
+      "**/public/**",
+      "**/node_modules/**",
+      "*.js",
+      "*.mjs",
+      "*.ts",
+      "!src/**"
+    ],
+  },
+];
